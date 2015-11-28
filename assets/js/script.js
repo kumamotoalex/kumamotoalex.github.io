30779f7bfa0bba5c95b9a0aeeecfae4a343bcebf
Alexander Kumamoto <kumamotoalex@berkeley.edu>
2015-11-27 22:49:34 -0800

    Created random name generating website

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+
+	$(".info-button").click(function() {
+		var x = $(".info-box").val();
+		var result = "";
+		var arrlow = ["a","e","i","o","u","ea","oo","ou","ee","ai"];
+		var arrhigh = ["A","E","I","O","U"];
+		for (i = 0; i < x.length; i++) {
+			var c = x.charAt(i);
+			if (jQuery.inArray(c, arrlow) != -1) {
+				var index = Math.floor(Math.random() * 10);
+				result += arrlow[index];
+				
+			} else {
+				result += c;
+			}
+		}
+		$("#right-side").text(result);
+	});
+	
+});

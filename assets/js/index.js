21d6216d15eb1c4fc78e6e897d83637a282e1bd2
Alexander Kumamoto <kumamotoalex@berkeley.edu>
2015-11-29 15:52:05 -0800

    before changing to gradient style

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
 	
 
 	$(".resume-container").hover(function() {
+		var numer = ["one","two","three","four","five","six"];
+		var color = ["#1fcecb", "#33cc33", "#ff6600", "#ff5050", "#ff66cc", "#3333ff", "#1fcecb"];
 		var b = $("#" + $(this).attr('id') + "-dne").css("color");
 		$("#" + $(this).attr('id')).css("color","black");
 		$("#" + $(this).attr('id')).css("background-color","white");
-		$("#background-overlay").css("background", b);
+		// $("#background-overlay").css("background", b);
+
+
 		$(".resume-spacer").css("color", "white");
 	});
 
